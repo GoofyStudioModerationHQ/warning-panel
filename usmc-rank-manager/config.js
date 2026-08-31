5085d4dba80bb57d94fa42c677b1e541147c0e3f
GoofyStudio <peterthecoolest21@gmail.com>
2026-08-31 19:36:51 +0800

    Add USMC Rank Manager config.js

---
+++ usmc-rank-manager/config.js
@@ -0,0 +1,5 @@
+window.USMC_CONFIG = {
+  supabaseUrl: "https://zrntyxwlcfrzkautvlls.supabase.co",
+  publishableKey: "sb_publishable_m7hh1zxDX4uQVE50EMxC0g_3Mt4TTo8",
+  functionName: "rank-api"
+};
